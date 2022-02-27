856b46a8d012cdd00f4731cd21161cca392697f5
KostaLaz <kosta.lazarevski89@gmail.com>
2022-02-27 15:41:26 +0100

    Login added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './components/Login';
 
 function App() {
   return (
     <div className="App">
     <Router>
-      <Switch>
-        <Route exact path='/'>
-          {/* <Login /> */}
+      <Routes>
+        <Route exact path="/" element={<Login />}>
         </Route>
-      </Switch>
+      </Routes>
     </Router>
     </div>
   );

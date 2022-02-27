49682a5ca4cf19a6bc143b896a6e65d7de43e579
KostaLaz <kosta.lazarevski89@gmail.com>
2022-02-27 17:25:05 +0100

    Signup component added

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,15 @@ const Login = (props) => {
   return (
     <Container>
         <Content>
+          <CTA>
+            <CTALogoOne src="/images/cta-logo-one.svg" alt="" />
+            <SignUp>GET ALL THERE</SignUp>
+            <Description>
+              Get Premier Access to Raya and the Last Dragon for an additional fee
+              with a Disney+ subscription. As of 03/26/21, the price of Disney+
+              and The Disney Bundle will increase by $1.
+            </Description>
+          </CTA>
             <BackgroundImage />
         </Content>
     </Container>
@@ -45,5 +54,46 @@ const BackgroundImage = styled.div`
   left: 0;
   z-index: -1;
 `;
+
+const CTA = styled.div`
+  margin-bottom: 2vw;
+  max-width: 650px;
+  display: flex:
+  flex-wrap: wrap;
+  flex-direction: column:
+  justify-content: center;
+  margin-top: 0;
+  margin-right: auto;
+  margin-left: auto;
+  align-items: center;
+  transition-timing-function: ease-out;
+  transition: opacity .2s;
+  width: 100%;
+`;
+
+const CTALogoOne = styled.img`
+  margin-bottom: 12px;
+  max-width: 600px;
+  min-height: 1px;
+  display: block;
+  width: 100%;
+  `;
+
+  const SignUp = styled.a`
+    font-weight: bold;
+    color: #f9f9f9;
+    background-color: #0063e5;
+    margin-bottom: 12px;
+    width: 100%;
+    letter-spacing: 1.5px;
+    font-size: 18px;
+    padding: 16.5px;
+    border: 1px solid transparent;
+    border-radius: 4px;
+    
+    &:hover {
+      background-color: #0483ee;
+    } 
+  `;
 
 export default Login;
